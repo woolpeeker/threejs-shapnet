3abeb4324f9941d717bf4ad0a6f858e66f68492e
luojiapeng <luojiapeng1993@gmail.com>
2020-06-11 18:47:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## use three.js to render shapenetCore.v2
+
+### requirements
+nodejs==v.10.13.0
+three==3.3.0
+puppeteer==0.117.1
+
+### Usage
+Install
+```
+npm init
+npm install three@3.3.0
+npm install puppeteer@0.117.1
+```
+
+Prepare the ShapeNetCore.v2.json to replace the Demo.json
+
+```
+node threejs_render.js
+```
---
+++ threejs_render.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const path = require('path')
+
+
+var CATEGORIES = ['02691156'];
+var shapenetRoot = 'ShapeNetCore.v2'
+// var shapenetMeta = 'ShapeNetDemo.json';
+var shapenetMeta = 'ShapeNetCore.v2.json';
+var outRoot = 'output';
+
+
+var file_meta = fs.readFileSync(shapenetMeta);
+file_meta = JSON.parse(file_meta);
+
+async function work_fn(){
+    const browser = await puppeteer.launch({ headless: false });
+    for (let i=0; i<CATEGORIES.length; i++) {
+        let cate = CATEGORIES[i]
+        for (let j=0; j<file_meta[cate].length; j++) {
+            let modelName = file_meta[cate][j];
+            let token = `${shapenetRoot}/${cate}/${modelName}/models/model_normalized`;
+            // let token = 'tmp/models/model_normalized';
+            let outpath = `${outRoot}/${cate}/${modelName}`;
+            mkdirsSync(outpath);
+            await render_model(browser, token, outpath);
+        }
+    }
+    // await browser.close();
+}
+
+async function render_model(browser, token, out_dir) {
+    const page = await browser.newPage();
+    await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: out_dir });
+    await page.goto(`http://127.0.0.1:8000/screenshot.html?token=${token}`);
+    await page.waitFor('#finished');
+    await page.close();
+}
+
+function mkdirsSync(dirname) {
+    if (fs.existsSync(dirname)) {
+        return true;
+    } else {
+        if (mkdirsSync(path.dirname(dirname))) {
+            fs.mkdirSync(dirname);
+            return true;
+        }
+    }
+}
+
+work_fn();
